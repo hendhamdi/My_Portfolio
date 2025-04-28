cf0d8748745d57aa4224f08c2e61f4072bc01543
unknown <hendh0487@gmail.com>
2025-04-28 16:42:29 +0100

    update cv

---
+++ components/Experience/Experience.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./Experience.module.css";
+import skills from "../../data/skills.json";
+import { getImageUrl } from "../../utils";
+
+export const Experience = () => {
+  return (
+    <section className={styles.container} id="experience">
+      <p className={styles.subtitle}>Discover My Technical Expertise</p>
+      <h2 className={styles.title}>HARD-SKILLS</h2>
+      <div className={styles.skillsGrid}>
+        {skills.map((skill, index) => (
+          <div key={index} className={styles.skillCard}>
+            <div className={styles.skillImage}>
+              <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+            </div>
+            <p className={styles.skillTitle}>{skill.title}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
---
+++ components/Projects/Projects.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./Projects.module.css";
+import projects from "../../data/projects.json";
+import { ProjectCard } from "./ProjectCard";
+
+export const Projects = () => {
+  return (
+    <section className={styles.container} id="projects">
+      <h2 className={styles.title}>My Development & Design Projects</h2>
+
+      <h3 className={styles.subTitle}>Development Projects</h3>
+      <div className={styles.projects}>
+        {projects.devProjects.map((project, id) => (
+          <ProjectCard key={id} project={project} />
+        ))}
+      </div>
+
+      <h3 className={styles.subTitle}>Design Projects</h3>
+      <div className={styles.projects}>
+        {projects.designProjects.map((project, id) => (
+          <ProjectCard key={id} project={project} />
+        ))}
+      </div>
+    </section>
+  );
+};
